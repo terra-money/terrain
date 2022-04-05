d2f84c260c16e94fbc95a9407cfc3b78024a4092
Artem <rtkushner@gmail.com>
2022-04-05 10:45:58 -0700

    Removed cargo-generate from "new" command.

--- src/commands/code/new.ts
+++ src/commands/code/new.ts
@@ -1,6 +1,9 @@
 import { Command, flags } from "@oclif/command";
 import { execSync } from "child_process";
+import * as fs from "fs-extra";
+import * as filesys from "fs";
 import { cli } from "cli-ux";
+import * as path from "path";
 
 export default class CodeNew extends Command {
   static description = "Generate new contract.";
@@ -23,9 +26,41 @@ export default class CodeNew extends Command {
     process.chdir(flags.path);
 
     cli.action.start("generating contract");
-    execSync(
-      `cargo generate --git https://github.com/CosmWasm/cw-template.git --branch ${flags.version} --name ${args.name}`
-    );
+
+    // ------------------------------- Contract template
+    // https://github.com/InterWasm/cw-template/archive/refs/heads/main.zip
+    const repoName     = "cw-template"
+    // const branch       = "main";
+    const branch      = "0.16";
+    const contractName = "counter";
+
+    const contractFile = fs.createWriteStream("contract.zip");
+    await new Promise((resolve, reject) => {
+      request
+        .get(`https://github.com/InterWasm/${repoName}/archive/refs/heads/${branch}.zip`)
+        .on("error", (error) => {reject(error);})
+        .pipe(contractFile)
+        .on("finish", () => {cli.action.stop();resolve(null);});
+    });
+
+    const contractZip = new Zip("contract.zip");
+    contractZip.extractAllTo(".", true);
+
+    fs.renameSync(`${repoName}-${branch}`, `${contractName}`);
+    fs.removeSync("contract.zip");
+    // Postprocess Cargo.toml - name & authors
+    const Cargotoml = path.resolve(process.cwd(), `${contractName}`,'Cargo.toml')
+    filesys.readFile(Cargotoml, {encoding: 'utf8'},  (e:any,data:any) =>{
+      if (e) { throw e };
+
+      var _ = data
+      .replace(/name = "{{project-name}}"/g, 'name = "terrain-counter-template"')
+      .replace(/authors = \["{{authors}}"\]/g, 'authors = ["terrain-developer"]')
+      
+      filesys.writeFile(Cargotoml, _, 'utf-8', function (err) {
+          if (err) throw err;
+      });
+    });
 
     cli.action.stop();
   }
