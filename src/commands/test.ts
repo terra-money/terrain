2756fe7bf6d3454c8d66c496bb6978b918b9576e
Eric Gorski <eric@MacBook-Pro.local>
2022-04-05 16:02:46 -0400

    wip: no fast fail flag

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,19 +1,30 @@
-import { Command } from "@oclif/command";
+import { Command, flags } from "@oclif/command";
 import { execSync } from "child_process";
 export default class Test extends Command {
-  static description = "Run unit tests for a contract.";
+  static description = "Runs unit tests for a contract directory.";
 
   static examples = [
     "$ terrain test counter",
+    "$ terrain test counter --no-fail-fast",
   ];
+
+  static flags = {
+    "--no-fail-fast": flags.string({ description: "Run all tests regardless of failure." })
+  };
 
   static args = [{ name: "name", required: true }];
 
   async run() {
     const { args, flags } = this.parse(Test);
 
+    process.chdir(`contracts/${args.name}`);
+    
+
+    // include --no-fail-fast as optional flag 
+
+
     execSync(
-      `cargo test contracts/${args.name}`
+      `cargo test ${flags["--no-fail-fast"]}`, { stdio: "inherit" }
     );
   }
 }
