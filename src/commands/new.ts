4a46f4389cfc0ca70ecf018618ba98ffc52fbaba
Eric Gorski <er.h.gorski@gmail.com>
2022-04-25 17:33:46 -0400

    feat: add support for external templates

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -2,7 +2,10 @@ import { Command, flags } from "@oclif/command";
 import { execSync } from "child_process";
 import * as fs from "fs-extra";
 import cli from "cli-ux";
+import * as request from "superagent";
+import * as Zip from "adm-zip";
 import * as path from "path";
+
 export default class New extends Command {
   static description = "Create new dapp from a template.";
 
@@ -16,7 +19,7 @@ export default class New extends Command {
   static flags = {
     path: flags.string({ description: "path to keep the project" }),
     framework: flags.string({
-      description: "frontend framework you want to use",
+      description: "Choose the frontend framework you want to use. Non-react framework options have better wallet-provider support but less streamlined contract integration.",
       options: ["next", "vue", "vite", "lit", "svelte", "react"],
       default: "react",
     }),
@@ -28,6 +31,7 @@ export default class New extends Command {
   static args = [{ name: "name", required: true }];
 
   async run() {
+    cli.log("CORRECT TESTING ONE: ");
     const { args, flags } = this.parse(New);
 
     cli.log("generating: ");
@@ -51,12 +55,33 @@ export default class New extends Command {
     process.chdir("..");
 
     cli.action.start("- frontend");
-    execSync(
-      `npx copy-github-directory https://github.com/terra-money/wallet-provider/tree/main/templates/${flags.framework} frontend`
-    )
-    process.chdir("frontend");
-
-    fs.removeSync("sandbox.config.json");
+    if (flags.framework === 'react') {
+      await new Promise((resolve, reject) => {
+        const file = fs.createWriteStream("frontend.zip");
+        request
+          .get(
+            "https://github.com/terra-money/terrain-frontend-template/archive/refs/heads/feat/typescript-conversion.zip"
+          )
+          .on("error", (error) => {
+            reject(error);
+          })
+          .pipe(file)
+          .on("finish", () => {
+            cli.action.stop();
+            resolve(null);
+          });
+      });
+      const zip = new Zip('frontend.zip');
+      zip.extractAllTo('.', true);
+      fs.renameSync('terrain-frontend-template-feat-typescript-conversion', 'frontend');
+      fs.removeSync('frontend.zip');
+    } else {
+      execSync(
+        `npx copy-github-directory https://github.com/terra-money/wallet-provider/tree/main/templates/${flags.framework} frontend`
+      );
+      process.chdir("frontend");
+      fs.removeSync("sandbox.config.json");
+    }
 
     cli.action.stop();
     fs.copySync(path.join(__dirname, "..", "template"), process.cwd());
