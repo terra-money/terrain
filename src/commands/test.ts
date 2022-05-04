12757fe12c64c8012795d73e096a506ce9882d09
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-05-04 09:51:39 -0700

    style: Cleaning up code.

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,5 +1,8 @@
 import { Command, flags } from "@oclif/command";
 import { execSync } from "child_process";
+import { range } from "ramda";
+import { existsSync } from 'fs';
+
 export default class Test extends Command {
   static description = "Runs unit tests for a contract directory.";
 
@@ -15,40 +18,44 @@ export default class Test extends Command {
   static args = [{ name: "contract-name", required: true }];
 
   async run() {
-    const sep = '=================================================================';
+    const sep = '\n=================================================================\n';
     const { args, flags } = this.parse(Test);
 
     // Specify default contract path from terrain project root directory.
     let contractPath = `contracts/${args["contract-name"]}`;
 
     // Backtrack through file tree to find contract directory.
-    const fs = require('fs');
-    while (!fs.existsSync(contractPath) && contractPath.slice(0, 12) !== '../../../../') {
+    for (let step_back = 1; step_back <=4; step_back++) {
 
+      // If contractPath available, alert user of testing initialization, 
+      // change working directory to contractPath and execute cargo test command.
+      if (existsSync(contractPath)) {
+        console.log(`${sep}\nTesting '${args["contract-name"]}' contract.\n${sep}`);
+        process.chdir(contractPath);
+        execSync(
+          `cargo test ${flags["no-fail-fast"] ? "--no-fail-fast" : ""}`, { stdio: "inherit" }
+        );
+        process.exit();
+      }
+  
       // If contracts directory available, but contractPath is not, 
       // then contract name referenced in terrain test command is invalid.
-      if (fs.existsSync('contracts/')) {
-        console.error(`\n${sep}\n\nERROR: Contract '${args["contract-name"]}' not available in contracts directory.\n\n${sep}\n`)
-        process.exit()
+      if (existsSync('contracts/') && !existsSync(contractPath)) {
+        console.error(
+          `${sep}\nERROR: Contract '${args["contract-name"]}' not available in contracts directory.\n${sep}`
+        );
+        process.exit();
       }
 
-      // If contractPath does not exist in current directory, move back one directory.
+      // If contracts directory does not exist in current directory, step back one directory.
       contractPath = '../' + contractPath;
     }
 
     // If contractPath not found after moving back 4 directories, 
     // tell user to run command in a terrain project directory.
-    if (!fs.existsSync(contractPath)) {
-      console.error(`\n${sep}\n\nERROR: Please ensure that you are in a terrain project directory.\n\n${sep}\n`)
-      process.exit()
-    }
-
-    // If contractPath available, alert user of testing initialization, 
-    // change working directory to contractPath and execute cargo test command.
-    console.log(`\n${sep}\n\nTesting '${args["contract-name"]}' contract.\n\n${sep}\n`)
-    process.chdir(contractPath);
-    execSync(
-      `cargo test ${flags["no-fail-fast"] ? "--no-fail-fast" : ""}`, { stdio: "inherit" }
+    console.error(
+      `${sep}\nERROR: Please ensure that you are in a terrain project directory.\n${sep}`
     );
+    process.exit();
   }
 }
