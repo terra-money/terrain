3191a2e00ff96de7eedafbbe7072b1119e245a9b
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-05-03 18:00:54 -0700

    feat: Run terrain test command from anywhere inside Terrain project

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -14,11 +14,39 @@ export default class Test extends Command {
 
   static args = [{ name: "contract-name", required: true }];
 
-  async run() { // TODO use fs.existsSync() to call terrain test from any project directory level
+  async run() {
+    const sep = '=================================================================';
     const { args, flags } = this.parse(Test);
 
-    process.chdir(`contracts/${args["contract-name"]}`);
+    // Specify default contract path from terrain project root directory.
+    let contractPath = `contracts/${args["contract-name"]}`;
 
+    // Backtrack through file tree to find contract directory.
+    const fs = require('fs');
+    while (!fs.existsSync(contractPath) && contractPath.slice(0, 12) !== '../../../../') {
+
+      // If contracts directory available, but contractPath is not, 
+      // then contract name referenced in terrain test command is invalid.
+      if (fs.existsSync('contracts/')) {
+        console.error(`\n${sep}\n\nERROR: Contract '${args["contract-name"]}' not available in contracts directory.\n\n${sep}\n`)
+        process.exit()
+      }
+
+      // If contractPath does not exist in current directory, move back one directory.
+      contractPath = '../' + contractPath;
+    }
+
+    // If contractPath not found after moving back 4 directories, 
+    // tell user to run command in a terrain project directory.
+    if (!fs.existsSync(contractPath)) {
+      console.error(`\n${sep}\n\nERROR: Please ensure that you are in a terrain project directory.\n\n${sep}\n`)
+      process.exit()
+    }
+
+    // If contractPath available, alert user of testing initialization, 
+    // change working directory to contractPath and execute cargo test command.
+    console.log(`\n${sep}\n\nTesting '${args["contract-name"]}' contract.\n\n${sep}\n`)
+    process.chdir(contractPath);
     execSync(
       `cargo test ${flags["no-fail-fast"] ? "--no-fail-fast" : ""}`, { stdio: "inherit" }
     );
