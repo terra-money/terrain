887fbe982f61a44e5c2807ea77add27cee7993ff
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-05-05 15:34:53 -0700

    style: ESLint fixes

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,38 +1,37 @@
-import { Command, flags } from "@oclif/command";
-import { execSync } from "child_process";
+import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
 import { existsSync } from 'fs';
 import TerrainCLI from '../TerrainCLI';
 
 export default class Test extends Command {
-  static description = "Runs unit tests for a contract directory.";
+  static description = 'Runs unit tests for a contract directory.';
 
   static examples = [
-    "$ terrain test counter",
-    "$ terrain test counter --no-fail-fast",
+    '$ terrain test counter',
+    '$ terrain test counter --no-fail-fast',
   ];
 
   static flags = {
-    "no-fail-fast": flags.boolean({ description: "Run all tests regardless of failure." })
+    'no-fail-fast': flags.boolean({ description: 'Run all tests regardless of failure.' })
   };
 
-  static args = [{ name: "contract-name", required: true }];
+  static args = [{ name: 'contract-name', required: true }];
 
   async run() {
     const { args, flags } = this.parse(Test);
 
     // Specify default contract path from terrain project root directory.
-    let contractPath = `contracts/${args["contract-name"]}`;
+    let contractPath = `contracts/${args['contract-name']}`;
 
     // Backtrack through file tree to find contract directory.
     for (let stepBack = 1; stepBack <= 4; stepBack++) {
-
       // If contractPath available, alert user of testing initialization, 
       // change working directory to contractPath and execute cargo test command.
       if (existsSync(contractPath)) {
-        TerrainCLI.log(`Testing '${args["contract-name"]}' contract.`);
+        TerrainCLI.log(`Testing '${args['contract-name']}' contract.`);
         process.chdir(contractPath);
         execSync(
-          `cargo test ${flags["no-fail-fast"] ? "--no-fail-fast" : ""}`, { stdio: "inherit" }
+          `cargo test ${flags['no-fail-fast'] ? '--no-fail-fast' : ''}`, { stdio: 'inherit' }
         );
         process.exit();
       }
@@ -41,7 +40,7 @@ export default class Test extends Command {
       // then contract referenced in terrain test command does not exist.
       if (existsSync('contracts/') && !existsSync(contractPath)) {
         TerrainCLI.error(
-          `Contract '${args["contract-name"]}' not available in contracts directory.`
+          `Contract '${args['contract-name']}' not available in contracts directory.`
         );
         process.exit();
       }
