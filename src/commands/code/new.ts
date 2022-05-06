d409d9ad0f933d6a08d8853b68fbd1727406b208
Emilian Harastasan <emilian@terra.money>
2022-05-06 17:52:00 +0200

    wip: template-scaffolding

--- src/commands/code/new.ts
+++ src/commands/code/new.ts
@@ -1,20 +1,21 @@
 import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
 import { cli } from 'cli-ux';
 import { TemplateScaffolding } from '@terra-money/template-scaffolding';
 import * as path from 'path';
-import * as fs from 'fs';
 
 export default class CodeNew extends Command {
   static description = 'Generate new contract.';
-  
+
   static examples = [
-    '$ terrain code:new helloworld',
-    '$ terrain code:new helloworld --authors ExampleAuthor<example@email.domain>',
+    '$ terrain code:new awesome_contract',
+    '$ terrain code:new awesome_contract --path path/to/dapp',
+    '$ terrain code:new awesome_contract --path path/to/dapp --authors ExampleAuthor<example@email.domain>',
   ];
 
   static flags = {
     path: flags.string({
-      description: 'Path to create the contracts',
+      description: 'path to keep the contracts',
       default: './contracts',
     }),
     version: flags.string({
@@ -25,40 +26,28 @@ export default class CodeNew extends Command {
     }),
   };
 
-  static args = [{ 
-    name: 'name', 
-    required: true 
-  }];
+  static args = [{ name: 'name', required: true }];
 
   async run() {
     const { args, flags } = this.parse(CodeNew);
-    const contractPath = path.join(process.cwd(), flags.path, args.name);
 
-    if(fs.existsSync(contractPath)){
-      throw Error(
-        `Folder '${args.name}' already exists under path '${flags.path}'.\nTip: Use another contract name`
-      );
-    }
-
-    cli.log(`generating ${args.name}:`);
+    cli.log(`generating contract ${args.name}:`);
     cli.action.start('- contract');
-
-    TemplateScaffolding.from({
+    await TemplateScaffolding.from({
       remoteUrl: `https://codeload.github.com/InterWasm/cw-template/zip/refs/heads/${flags.version}`,
+      subFolder: `cw-template-${flags.version}`,
       localOptions: {
-        folderUrl: path.join(process.cwd(), flags.path, args.name),
-        toRootFolderUrl: true
+        folderUrl: path.join(process.cwd(), flags.path, args.name, "contracts", args.name)
       },
       replace: {
         entries: {
-          "project-name" : "counter",
-          "crate_name" : "counter",
-          "authors" : flags.authors,
+          "project-name": args.name,
+          "crate_name": args.name,
+          "authors": flags.authors,
           " \"now\" | date: \"%Y\" ": `${new Date().getFullYear()}`
         }
       }
     });
-
     cli.action.stop();
   }
 }
