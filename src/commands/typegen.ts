51c27634fd2ad3c7169ee7b48539bc7a92a71c27
Artem <rtkushner@gmail.com>
2022-05-06 09:34:48 -0700

    fix(linter): fixed the linter errors

--- src/commands/typegen.ts
+++ src/commands/typegen.ts
@@ -1,108 +1,99 @@
-import { Command, flags } from "@oclif/command";
-import { execSync } from "child_process";
-import * as path from 'path'
-import namespaceGen from "../lib/namespace-gen";
-import * as fs from 'fs'
+import { Command, flags as oclifFlags } from '@oclif/command';
+import * as path from 'path';
+import * as fs from 'fs';
+import namespaceGen from '../lib/namespace-gen';
 
-const hl           = (_: string) => `\x1b[35m${_}\x1b[0m`
-const snakeToCamel = (_:string) =>
-  _.toLowerCase().replace(/([-_][a-z])/g, group =>
-    group
-      .toUpperCase()
-      .replace('-', '')
-      .replace('_', '')
-  );
+const hl = (_: string) => `\x1b[35m${_}\x1b[0m`;
+const snakeToCamel = (_: string) => _.toLowerCase().replace(/([-_][a-z])/g, (group) => group
+  .toUpperCase()
+  .replace('-', '')
+  .replace('_', ''));
 
 export default class GenerateTypes extends Command {
-    static description = `Generates .TS type definitions for a given CosmWasm contract. 
+  static description = `Generates .TS type definitions for a given CosmWasm contract. 
   Under development:[
     Each CosmWasm contract registered with terrain has its types exported as TypeScript type declarations.
     These declarations can be found in terrain/types as individual namespaces or reimported from the master Terrain namespace.
     ]
   `;
 
-    // -------------------------------------------------
-    // Oclif format the space out of its command 'description' output. 
-    // Here is how the typedefs housekeeping might look like.
-    // -------------------------------------------------
-    //     terrain
-    //     ├── ...
-    //     ├── tasks
-    //     ├── contracts
-    //     |   ├── counter
-    //     |   └── staking
-    //     └── types
-    //         ├── terrain-global.ts
-    //         ├── staking
-    //         |   └── staking.ts
-    //         └── counter
-    //             └── counter.ts
-    // -------------------------------------------------
+  // -------------------------------------------------
+  // Oclif formats the spaces out of its command 'description' output.
+  // Here is how the typedefs housekeeping might look like.
+  // -------------------------------------------------
+  //     terrain
+  //     ├── ...
+  //     ├── tasks
+  //     ├── contracts
+  //     |   ├── counter
+  //     |   └── staking
+  //     └── types
+  //         ├── terrain-global.ts
+  //         ├── staking
+  //         |   └── staking.ts
+  //         └── counter
+  //             └── counter.ts
+  // -------------------------------------------------
 
-    static examples = [
-        "$ terrain typegen mycontract",
-        "$ terrain typegen mycontract --outdir ~/mytypes",
-        "$ terrain typegen mycontract --outdir ~/mytypes --name customNamespace",
-    ];
+  static examples = [
+    '$ terrain typegen mycontract',
+    '$ terrain typegen mycontract --outdir ~/mytypes',
+    '$ terrain typegen mycontract --outdir ~/mytypes --name customNamespace',
+  ];
 
-    static flags = {
-        "outdir": flags.string({ description: "Custom output directory." }),
-        "name"  : flags.string({description:"Name to give to the generated namespace and its containing file."})
-    };
+  static flags = {
+    outdir: oclifFlags.string({ description: 'Custom output directory.' }),
+    name: oclifFlags.string({ description: 'Name to give to the generated namespace and its containing file.' }),
+  };
 
-    static args = [{
-        name       : "contractDir",
-        required   : true,
-        description: "Provide a path to a CosmWasm contract directory containing a 'schema' folder."
-    }];
+  static args = [{
+    name: 'contractDir',
+    required: true,
+    description: "Provide a path to a CosmWasm contract directory containing a 'schema' folder.",
+  }];
 
-    async run() {
-        const { args, flags } = this.parse(GenerateTypes);
+  async run() {
+    const { args, flags } = this.parse(GenerateTypes);
 
-        const CONTRACT_DIR = args.contractDir
-        const OUT_DIR      = flags.outdir
-        const OUT_NAME     = flags.name
+    const CONTRACT_DIR = args.contractDir;
+    const OUT_DIR = flags.outdir;
+    const OUT_NAME = flags.name;
 
-        //  verifying input  
-        try {
+    //  verifying input
+    try {
+      fs.lstatSync(CONTRACT_DIR).isDirectory();
+      fs.lstatSync(path.resolve(CONTRACT_DIR, 'schema')).isDirectory();
 
-            fs.lstatSync(CONTRACT_DIR).isDirectory()
-            fs.lstatSync(path.resolve(CONTRACT_DIR, 'schema')).isDirectory()
-
-            fs.readdirSync(path.resolve(CONTRACT_DIR, 'schema')).length < 1
-                ? console.error(`The schema directory is empty. Generating an empty namespace.
-Consider generating CosmWasm schema manually: ${hl("cargo schema")}
-https://docs.cosmwasm.com/tutorials/simple-option/develop/#schema`)
-                : null
-
-        } catch (e) {
-            console.error(`${hl(CONTRACT_DIR)} is NOT a valid CosmWasm contract directory.`)
-            process.exit(1)
-        }
-
-        if (OUT_DIR !== undefined) {
-            try {
-                fs.lstatSync(OUT_DIR).isDirectory()
-            } catch (direrorr) {
-                console.error(`"${hl(OUT_DIR)}" either does not exist or is NOT a valid directory. Provide a valid path to output a .TS namespace to.`);
-                process.exit(1)
-            }
-        } else {
-            // ? TODO: Implement a Terain master namespace reimporting types for all registered contracts.
-            console.log("[Terrain] Freeform invocation unimplemented. Use with '--outdir [DIR]' for now")
-            process.exit(1)
-        }
-
-
-        let   namespaceName = OUT_NAME || snakeToCamel(path.basename(CONTRACT_DIR))
-        const result        = await namespaceGen(namespaceName, args.contractDir)
-
-        try{
-            fs.writeFileSync(path.resolve(OUT_DIR, `${namespaceName}.ts`), result)
-            console.warn(`[Terrain]: Wrote to ${hl(path.resolve(OUT_DIR, `${namespaceName}.ts`))} successfully.`)
-        } catch (_){
-            console.warn(`[Terrain]: Could not write to ${path.resolve(OUT_DIR, `${namespaceName}.ts`)}.`)
-            throw _
-        }
+      if (fs.readdirSync(path.resolve(CONTRACT_DIR, 'schema')).length < 1) {
+        console.warn(`The schema directory is empty. Generating an empty namespace.\nConsider generating CosmWasm schema manually: ${hl('cargo schema')} https://docs.cosmwasm.com/tutorials/simple-option/develop/#schema`);
+      }
+    } catch (e) {
+      console.error(`${hl(CONTRACT_DIR)} is NOT a valid CosmWasm contract directory.`);
+      process.exit(1);
     }
+
+    if (OUT_DIR !== undefined) {
+      try {
+        fs.lstatSync(OUT_DIR).isDirectory();
+      } catch (direrorr) {
+        console.error(`"${hl(OUT_DIR)}" either does not exist or is NOT a valid directory. Provide a valid path to output a .TS namespace to.`);
+        process.exit(1);
+      }
+    } else {
+      // ? TODO: Implement a Terain master namespace reimporting types for all registered contracts.
+      console.log("[Terrain] Freeform invocation unimplemented. Use with '--outdir [DIR]' for now");
+      process.exit(1);
+    }
+
+    const namespaceName = OUT_NAME || snakeToCamel(path.basename(CONTRACT_DIR));
+    const result = await namespaceGen(namespaceName, args.contractDir);
+
+    try {
+      fs.writeFileSync(path.resolve(OUT_DIR, `${namespaceName}.ts`), result);
+      console.warn(`[Terrain]: Wrote to ${hl(path.resolve(OUT_DIR, `${namespaceName}.ts`))} successfully.`);
+    } catch (_) {
+      console.warn(`[Terrain]: Could not write to ${path.resolve(OUT_DIR, `${namespaceName}.ts`)}.`);
+      throw _;
+    }
+  }
 }
--- src/lib/namespace-gen.ts
+++ src/lib/namespace-gen.ts
@@ -1,57 +1,74 @@
-import { compileFromFile } from 'json-schema-to-typescript';
 import * as fs from 'fs';
+import { compileFromFile } from 'json-schema-to-typescript';
 import * as path from 'path';
 
-/**
- * This is a work in progress and we should decide how this factors into the Terrain workflow:
- * - to create namespace when generating a fronted template
- * - as a custom command to execute
- * 1. Point at this at a contract folder ==> $CONTRACT_NAME.
- * 2. Generate a namespace file with the declarations corresponding to cosmwasm schema files.
- * 3. Write to specified directory or add to the common Terrain namespace.
- * //----------------------------------------------------------------------
- * We should decide how to structure this inside Terrain:
- * if we decide to keep all contracts' declarations under a common namespace, say DTerrain,
- * then the user could also share namespaces in multi-contract code for example.
- */
+const hl = (_: string) => `\x1b[35m${_}\x1b[0m`;
 
 /**
  * Given a CosmWasm smart contract directory with a schema folders, generate type declrataions
  * corresponding to the json schema.
  * @param {number} namespaceName Name to assign to the containing file and the namespace.
  * under which the declaraction are exported.
- * @param {number} contractPath path to the CosmWasm smart contract. This ought to contain 
+ * @param {number} contractPath path to the CosmWasm smart contract. This ought to contain
  * a 'schema' folder.
  * @returns {string}  the generated declarations.
  */
-
 const namespaceGen = async (
-    namespaceName: string,
-    contractPath : string): Promise<string> => {
+  namespaceName: string,
+  contractPath: string,
+): Promise<string> => {
+  const schemaDir = path.resolve(contractPath, 'schema');
+  const schemaFiles = fs.readdirSync(schemaDir);
 
-    const schemaDir      = path.resolve(contractPath, 'schema')
-    const schemaFiles    = fs.readdirSync(schemaDir)
+  const tsDeclarations = schemaFiles.map(
+    (file) => compileFromFile(
+      path.resolve(schemaDir, file),
+      {
+        bannerComment: `//This type was automatically generated from ***${file}*** file in the ${schemaDir}.`,
+        strictIndexSignatures: true,
+        unknownAny: false,
+      },
+    ),
+  );
 
-    let   tsDeclarations = schemaFiles.map(
-        (file) => compileFromFile(path.resolve(schemaDir, file),
-        {
-            bannerComment        : `//This type was automatically generated from ***${file}*** file in the ${schemaDir}.`,
-            strictIndexSignatures: true,
-            unknownAny           : false,
-        }))
-
-    const tsDecWrapInNamespace = (declarations: string) =>
-                                 (contractName: string) => `
+  const tsDecWrapInNamespace = (declarations: string) => (contractName: string) => `
 
 // Read more about CosmWasm schema: https://github.com/CosmWasm/cosmwasm/tree/main/packages/schema.
 // Pattern  "[k: string]: any | undefined;" signfifies a field/value can be left empty. 
 export namespace ${contractName} {\n${declarations}}`;
 
-    return Promise.all(tsDeclarations)
-        .then((_) => _.reduce((prev: string, acc: string) => { return acc.concat("\n", prev) },""))
-        .then(contractDeclarations => tsDecWrapInNamespace(contractDeclarations)(namespaceName));
-}
+  const alldecs: string[] = [];
+  (await Promise.all(tsDeclarations)).map((d) => alldecs.push(d));
+
+  /**
+           * For every generated typedef:
+           * 1. Locate 'export' line
+           * 2. Excise words reserved by TypeScript between the 'export' and the its name.
+           * 3. Assert that there are no duplicate exports.
+           * */
+  const REG = /^(?![/|*]).*\b(export)\b.*$/gm;
+  let IDENTIFIERS: Array<string> = [];
+
+  alldecs.map((declaration) => {
+    const exports = declaration.match(REG) || [];
+    const _ = exports.map(
+      (line: string) => line.replace(/(object|enum|type|interface|package|abstract|class|function|default)/gm, '')
+        .trim()
+        .split(/\s+/)[1],
+    );
+    IDENTIFIERS = [...IDENTIFIERS, ..._];
+    return null;
+  });
+
+  // If there are duplicates -- locate them and throw.
+  if (IDENTIFIERS.length !== new Set(IDENTIFIERS).size) {
+    const dupes = IDENTIFIERS.filter(((s) => (v) => s.has(v) || !s.add(v))(new Set()));
+    console.error(`[Terrain]:${hl(schemaDir)} has duplicate identifiers declated: [${hl(dupes.join(','))}].\nMake sure ${hl('cargo schema')} creates unique types.`);
+    throw new SyntaxError();
+  }
+
+  return Promise.all(tsDeclarations)
+    .then((_) => _.reduce((prev: string, acc: string) => acc.concat('\n', prev), ''))
+    .then((contractDeclarations) => tsDecWrapInNamespace(contractDeclarations)(namespaceName));
+};
 export default namespaceGen;
-
-
-
