58bb5868ffa8d537650c0d6fe3d44f9f2c418d02
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-05-06 09:26:22 -0700

    style: Updating commenting and TerrainCLI class styling.

--- src/TerrainCLI.ts
+++ src/TerrainCLI.ts
@@ -1,16 +1,51 @@
 import { cli } from 'cli-ux';
 
+/** TerrainCLI offers default log styling for terrain commands. */
 class TerrainCLI {
-  error(body = '') {
-    cli.log(
-      `\n\x1b[33m${'>'}\x1b[0m \x1b[31mError: ${body}\x1b[0m\n`,
-    );
+  prefixColor: string;
+
+  logColor: string;
+
+  errorColor: string;
+
+  warningColor: string;
+
+  reset: string;
+
+  constructor(
+    prefixColor: string,
+    logColor: string,
+    errorColor: string,
+    warningColor:string,
+    reset: string,
+  ) {
+    this.prefixColor = prefixColor;
+    this.logColor = logColor;
+    this.errorColor = errorColor;
+    this.warningColor = warningColor;
+    this.reset = reset;
   }
+
+  // TerrainCLI.log() styling.
   log(body = '') {
     cli.log(
-      `\n\x1b[33m${'>'}\x1b[0m \x1b[32m${body}\x1b[0m\n`,
+      `\n${this.prefixColor}>${this.reset} ${this.logColor}${body}${this.reset}\n`,
+    );
+  }
+
+  // TerrainCLI.error() styling.
+  error(body = '') {
+    cli.log(
+      `\n${this.prefixColor}>${this.reset} ${this.errorColor}Error: ${body}${this.reset}\n`,
+    );
+  }
+
+  // TerrainCLI.warning() styling.
+  warning(body = '') {
+    cli.log(
+      `\n${this.prefixColor}>${this.reset} ${this.warningColor}Warning: ${body}${this.reset}\n`,
     );
   }
 }
 
-export default new TerrainCLI();
+export default new TerrainCLI('\x1b[37m', '\x1b[32m', '\x1b[31m', '\x1b[33m', '\x1b[0m');
--- src/commands/test.ts
+++ src/commands/test.ts
@@ -3,56 +3,56 @@ import { execSync } from 'child_process';
 import { existsSync } from 'fs';
 import TerrainCLI from '../TerrainCLI';
 
+/**
+ * Runs unit tests for a contract directory.
+ * Examples:
+ * $ terrain test counter
+ * $ terrain test counter --no-fail-fast
+ */
 export default class Test extends Command {
-  static description = 'Runs unit tests for a contract directory.';
-
-  static examples = [
-    '$ terrain test counter',
-    '$ terrain test counter --no-fail-fast',
-  ];
-
-  static flags = {
-    'no-fail-fast': flags.boolean({ description: 'Run all tests regardless of failure.' })
-  };
-
+  // Initialize args to be given after test command.
   static args = [{ name: 'contract-name', required: true }];
 
+  // Initialize flags, prefixed with '--' in a terminal command, to be given after args.
+  static flags = {
+    'no-fail-fast': flags.boolean({ description: 'Run all tests regardless of failure.' }),
+  };
+
   async run() {
+    // Extract args and flags specified in executed test command.
     const { args, flags } = this.parse(Test);
 
     // Specify default contract path from terrain project root directory.
     let contractPath = `contracts/${args['contract-name']}`;
 
-    // Backtrack through file tree to find contract directory.
-    for (let stepBack = 1; stepBack <= 4; stepBack++) {
-      // If contractPath available, alert user of testing initialization, 
+    // Backtrack, up to 4 times, through file tree to find contract directory.
+    for (let stepBack = 0; stepBack < 5; stepBack += 1) {
+      // If contractPath available, alert user of testing initialization,
       // change working directory to contractPath and execute cargo test command.
       if (existsSync(contractPath)) {
         TerrainCLI.log(`Testing '${args['contract-name']}' contract.`);
         process.chdir(contractPath);
-        execSync(
-          `cargo test ${flags['no-fail-fast'] ? '--no-fail-fast' : ''}`, { stdio: 'inherit' }
-        );
+        execSync(`cargo test ${flags['no-fail-fast'] ? '--no-fail-fast' : ''}`, { stdio: 'inherit' });
         process.exit();
       }
-  
-      // If contracts directory exists, but contractPath does not, 
+
+      // If contracts directory exists, but contractPath does not,
       // then contract referenced in terrain test command does not exist.
       if (existsSync('contracts/') && !existsSync(contractPath)) {
         TerrainCLI.error(
-          `Contract '${args['contract-name']}' not available in contracts directory.`
+          `Contract '${args['contract-name']}' not available in contracts directory.`,
         );
         process.exit();
       }
 
       // If contracts directory does not exist in current directory, step back one directory.
-      contractPath = '../' + contractPath;
+      contractPath = `../${contractPath}`;
     }
 
-    // If contractPath not found after stepping back 4 directories, 
+    // If contractPath not found after stepping back 4 directories,
     // tell user to run command in a terrain project directory.
-    TerrainCLI.error(
-      `Please ensure that you are in a terrain project directory.`
+    TerrainCLI.warning(
+      'Please ensure that you are in a terrain project directory.',
     );
     process.exit();
   }
