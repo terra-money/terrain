a6be184360d8bd31f32d0e1871290eb6e0148684
- <->
2022-05-10 22:17:25 +0200

    fix: linting for test and coverage

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,40 +1,38 @@
-import { Command, flags } from "@oclif/command";
-import { execSync } from "child_process";
-import * as path from "path";
-import * as fs from "fs";
+import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export default class Test extends Command {
-  static description = "Runs unit tests for a contract directory.";
+  static description = 'Runs unit tests for a contract directory.';
 
   static examples = [
-    "$ terrain test",
-    "$ terrain test counter",
-    "$ terrain test counter --no-fail-fast",
+    '$ terrain test',
+    '$ terrain test counter',
+    '$ terrain test counter --no-fail-fast',
   ];
 
   static flags = {
-    "no-fail-fast": flags.boolean({
-      description: "Run all tests regardless of failure.",
-      default: false
-    })
+    'no-fail-fast': flags.boolean({
+      description: 'Run all tests regardless of failure.',
+      default: false,
+    }),
   };
 
-  static args = [{ name: "contract-name" }];
+  static args = [{ name: 'contract-name' }];
 
   async run() {
     const { args, flags } = this.parse(Test);
-    const contractName = args["contract-name"];
+    const contractName = args['contract-name'];
     if (contractName) {
-      process.chdir(path.join("contracts", contractName));
+      process.chdir(path.join('contracts', contractName));
     }
 
     const exists = fs.existsSync('./Cargo.toml');
     if (!exists) {
-      throw Error(`Folder '${process.cwd()}' does not contain a smart contract.\nTip: Use another path or contract name`)
+      throw Error(`Folder '${process.cwd()}' does not contain a smart contract.\nTip: Use another path or contract name`);
     }
 
-    execSync(
-      `cargo test ${flags["no-fail-fast"] ? "--no-fail-fast" : ""}`, { stdio: "inherit" }
-    );
+    execSync(`cargo test ${flags['no-fail-fast'] ? '--no-fail-fast' : ''}`, { stdio: 'inherit' });
   }
 }
--- src/commands/test/coverage.ts
+++ src/commands/test/coverage.ts
@@ -1,35 +1,33 @@
-import { Command, flags } from "@oclif/command";
-import { execSync } from "child_process";
-import * as path from "path";
-import * as fs from "fs";
+import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export default class Test extends Command {
-  static description = "Runs unit tests for a contract directory.";
+  static description = 'Runs unit tests for a contract directory.';
 
   static examples = [
-    "$ terrain test:coverage",
-    "$ terrain test:coverage counter"
+    '$ terrain test:coverage',
+    '$ terrain test:coverage counter',
   ];
 
   static flags = { };
 
-  static args = [{ name: "contract-name" }];
+  static args = [{ name: 'contract-name' }];
 
   async run() {
     const { args, flags } = this.parse(Test);
-    const contractName = args["contract-name"];
+    const contractName = args['contract-name'];
 
     if (contractName) {
-      process.chdir(path.join("contracts", contractName));
+      process.chdir(path.join('contracts', contractName));
     }
 
     const exists = fs.existsSync('./Cargo.toml');
     if (!exists) {
-      throw Error(`Folder '${process.cwd()}' does not contain a smart contract.\nTip: Use another path or contract name`)
+      throw Error(`Folder '${process.cwd()}' does not contain a smart contract.\nTip: Use another path or contract name`);
     }
 
-    execSync(
-      `docker run --security-opt seccomp=unconfined -v "${process.cwd()}:/volume" xd009642/tarpaulin`, { stdio: "inherit" }
-    );
+    execSync(`docker run --security-opt seccomp=unconfined -v "${process.cwd()}:/volume" xd009642/tarpaulin`, { stdio: 'inherit' });
   }
 }
