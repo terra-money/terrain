665bc7ec0312ddc44975be10b3314efc866ee302
Jason Stallings <octalmage@gmail.com>
2022-07-14 13:20:17 -0500

    fix: Use relative imports for local packages.

--- src/lib/generateClient.ts
+++ src/lib/generateClient.ts
@@ -1,4 +1,5 @@
-import { generate, readSchemas } from '@terra-money/terra-cosmwasm-typescript-gen';
+// eslint-disable-next-line import/no-relative-packages
+import { generate, readSchemas } from '../../packages/cosmwasm-typescript-gen';
 
 export default async (name: string, schemaDir: string, out: string) => {
   const schemas = readSchemas({ schemaDir, argv: {} });
