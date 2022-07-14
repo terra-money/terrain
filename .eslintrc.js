8b0a3ceaab3cadc18b612d17dbf02e2d2ad519ba
Jason Stallings <jason@stallin.gs>
2022-07-14 16:44:07 -0500

    test: Get Jest and eslint updated.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,9 +16,17 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
   rules: {
     'import/extensions': 0,
     'import/no-unresolved': 0,
+    'no-underscore-dangle': 0,
+    'no-shadow': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
   },
+  ignorePatterns: ['/packages/**', 'jest.config.ts'],
 };
