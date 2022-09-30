3b291ee1159b5aa96123b409a1dc1016f8bdcb59
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-09-30 11:07:23 -0700

    feat: run main commands anywhere
    Run the following Terrain commands in any Terrain project subdirectory:
    `terrain console`
    `terrain deploy`
    `terrain sync-refs`

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,9 +1,13 @@
 import { Command, flags } from '@oclif/command';
 import { LCDClient } from '@terra-money/terra.js';
+import { join } from 'path';
+import { existsSync } from 'fs';
 import { loadConfig, loadConnections, loadGlobalConfig } from '../config';
 import { instantiate, storeCode } from '../lib/deployment';
 import { getSigner } from '../lib/signer';
 import * as flag from '../lib/flag';
+import TerrainCLI from '../TerrainCLI';
+import runCommand from '../lib/runCommand';
 
 export default class Deploy extends Command {
   static description = 'Build wasm bytecode, store code on chain and instantiate.';
@@ -28,82 +32,104 @@ export default class Deploy extends Command {
   async run() {
     const { args, flags } = this.parse(Deploy);
 
-    const connections = loadConnections(flags['config-path']);
-    const config = loadConfig(flags['config-path']);
-    const globalConfig = loadGlobalConfig(flags['config-path']);
-    const conf = config(flags.network, args.contract);
+    // Command execution path.
+    const execPath = 'config.terrain.json';
 
-    const lcd = new LCDClient(connections(flags.network));
-    const signer = await getSigner({
-      network: flags.network,
-      signerId: flags.signer,
-      keysPath: flags['keys-path'],
-      lcd,
-    });
+    // Command to be performed.
+    const command = async () => {
+      const connections = loadConnections(flags['config-path']);
+      const config = loadConfig(flags['config-path']);
+      const globalConfig = loadGlobalConfig(flags['config-path']);
+      const conf = config(flags.network, args.contract);
 
-    if (conf.deployTask) {
-      await this.config.runCommand('task:run', [
-        conf.deployTask,
-        '--signer',
-        flags.signer,
-        '--network',
-        flags.network,
-        '--refs-path',
-        flags['refs-path'],
-        '--config-path',
-        flags['config-path'],
-        '--keys-path',
-        flags['keys-path'],
-      ]);
-    } else {
-      // Store sequence to manually increment after code is stored.
-      const sequence = await signer.sequence();
-
-      const codeId = await storeCode({
-        lcd,
-        conf,
-        signer,
-        noRebuild: flags['no-rebuild'],
-        contract: args.contract,
+      const lcd = new LCDClient(connections(flags.network));
+      const signer = await getSigner({
         network: flags.network,
-        refsPath: flags['refs-path'],
-        useCargoWorkspace: globalConfig.useCargoWorkspace,
-      });
-
-      // pause for account sequence to update.
-      // eslint-disable-next-line no-promise-executor-return
-      await new Promise((r) => setTimeout(r, 1000));
-
-      const admin = flags['admin-address']
-        ? flags['admin-address']
-        : signer.key.accAddress;
-
-      await instantiate({
-        conf,
-        signer,
-        admin,
-        sequence: 1 + sequence,
-        contract: args.contract,
-        codeId,
-        network: flags.network,
-        instanceId: flags['instance-id'],
-        refsPath: flags['refs-path'],
+        signerId: flags.signer,
+        keysPath: flags['keys-path'],
         lcd,
       });
-    }
 
-    await this.config.runCommand('contract:generateClient', [
-      args.contract,
-      '--build-schema',
-    ]);
+      if (conf.deployTask) {
+        await this.config.runCommand('task:run', [
+          conf.deployTask,
+          '--signer',
+          flags.signer,
+          '--network',
+          flags.network,
+          '--refs-path',
+          flags['refs-path'],
+          '--config-path',
+          flags['config-path'],
+          '--keys-path',
+          flags['keys-path'],
+        ]);
+      } else {
+        // Store sequence to manually increment after code is stored.
+        const sequence = await signer.sequence();
 
-    if (!flags['no-sync']) {
-      await this.config.runCommand('sync-refs', [
-        '--refs-path',
-        flags['refs-path'],
-        '--dest',
-        flags['frontend-refs-path'],
+        const codeId = await storeCode({
+          lcd,
+          conf,
+          signer,
+          noRebuild: flags['no-rebuild'],
+          contract: args.contract,
+          network: flags.network,
+          refsPath: flags['refs-path'],
+          useCargoWorkspace: globalConfig.useCargoWorkspace,
+        });
+
+        // pause for account sequence to update.
+        // eslint-disable-next-line no-promise-executor-return
+        await new Promise((r) => setTimeout(r, 1000));
+
+        const admin = flags['admin-address']
+          ? flags['admin-address']
+          : signer.key.accAddress;
+
+        await instantiate({
+          conf,
+          signer,
+          admin,
+          sequence: 1 + sequence,
+          contract: args.contract,
+          codeId,
+          network: flags.network,
+          instanceId: flags['instance-id'],
+          refsPath: flags['refs-path'],
+          lcd,
+        });
+      }
+
+      await this.config.runCommand('contract:generateClient', [
+        args.contract,
+        '--build-schema',
       ]);
-    }
+
+      if (!flags['no-sync']) {
+        await this.config.runCommand('sync-refs', [
+          '--refs-path',
+          flags['refs-path'],
+          '--dest',
+          flags['frontend-refs-path'],
+        ]);
+      }
+    };
+
+    // Error check to be performed upon each backtrack iteration.
+    const errorCheck = () => {
+      if (existsSync('contracts') && !existsSync(join('contracts', args.contract))) {
+        TerrainCLI.error(
+          `Contract '${args.contract}' not available in 'contracts/' directory.`,
+        );
+      }
+    };
+
+    // Attempt to execute command while backtracking through file tree.
+    await runCommand(
+      execPath,
+      command,
+      errorCheck,
+    );
   }
 }
