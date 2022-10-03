77dd71d5fc6869e39a7c1ea3988d447a31bc3369
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-10-03 11:56:56 -0700

    feat: run main commands anywhere (#132)
    * feat: run main commands anywhere
    
    Run the following Terrain commands in any Terrain project subdirectory:
    `terrain console`
    `terrain deploy`
    `terrain sync-refs`
    
    * fix: log alert after LocalTerra connection
    
    Ensure successful connection to LocalTerra before logging start of command with LocalTerra.

--- src/commands/sync-refs.ts
+++ src/commands/sync-refs.ts
@@ -1,9 +1,11 @@
 import { Command } from '@oclif/command';
 import * as path from 'path';
 import { cli } from 'cli-ux';
+import { existsSync } from 'fs';
 import * as fs from 'fs-extra';
 import { refsPath, frontendRefsPath } from '../lib/flag';
 import TerrainCLI from '../TerrainCLI';
+import runCommand from '../lib/runCommand';
 
 export default class SyncRefs extends Command {
   static description = 'Sync configuration with frontend app.';
@@ -16,22 +18,40 @@ export default class SyncRefs extends Command {
   async run() {
     const { flags } = this.parse(SyncRefs);
 
-    if (!fs.pathExistsSync(flags.dest)) {
-      TerrainCLI.error(`Failed to sync refs. Destination directory '${flags.dest}' not found.`);
-    }
+    // Command execution path.
+    const execPath = flags.dest;
 
-    // Append "refs.terrain.json" to flags.dest path if file unavailable.
-    // The fs.copyFileSync command requires the full file path.
-    const destFullPath = flags.dest.endsWith('refs.terrain.json')
-      ? flags.dest
-      : path.join(flags.dest, 'refs.terrain.json');
+    // Command to be performed.
+    const command = async () => {
+      // Append "refs.terrain.json" to flags.dest path if file unavailable.
+      // The fs.copyFileSync command requires the full file path.
+      const destFullPath = flags.dest.endsWith('refs.terrain.json')
+        ? flags.dest
+        : path.join(flags.dest, 'refs.terrain.json');
 
-    cli.action.start(
-      `Syncing refs from '${flags['refs-path']}' to '${destFullPath}'`,
+      cli.action.start(
+        `Syncing refs from '${flags['refs-path']}' to '${destFullPath}'`,
+      );
+
+      fs.copyFileSync(flags['refs-path'], destFullPath);
+
+      cli.action.stop();
+    };
+
+    // Error check to be performed upon each backtrack iteration.
+    const errorCheck = () => {
+      if (existsSync(execPath) && !existsSync('refs.terrain.json')) {
+        TerrainCLI.error(
+          'refs.terrain.json file not available in project root directory.',
+        );
+      }
+    };
+
+    // Attempt to execute command while backtracking through file tree.
+    await runCommand(
+      execPath,
+      command,
+      errorCheck,
     );
-
-    fs.copyFileSync(flags['refs-path'], destFullPath);
-
-    cli.action.stop();
   }
 }
