1bc653629b89040c7677dd463c31045baa6882a2
Jason Stallings <jason@stallin.gs>
2022-10-10 16:38:46 -0500

    feat: TypeScript generation for CosmWasm 1.1.

--- packages/wasm-ast-types/src/types.ts
+++ packages/wasm-ast-types/src/types.ts
@@ -12,7 +12,7 @@ export interface Interface {
 
 export interface QueryMsg {
     $schema: string;
-    title: "QueryMsg";
+    title: "QueryMsg" | "QueryMsg_for_Empty";
     oneOf?: any;
     allOf?: any;
     anyOf?: any;
@@ -25,3 +25,43 @@ export interface ExecuteMsg {
     allOf?: any;
     anyOf?: any;
 }
+
+export interface JSONSchema {
+    $ref?: string;
+    $schema?: string;
+    additionalProperties?: boolean;
+    allOf?: JSONSchema[];
+    anyOf?: JSONSchema[];
+    definitions?: Record<string, JSONSchema>;
+    description?: string;
+    oneOf?: JSONSchema[];
+    properties?: Record<string, JSONSchema>;
+    patternProperties?: Record<string, JSONSchema>;
+    items?: JSONSchema | JSONSchema[];
+    additionalItems?: JSONSchema;
+    required?: string[];
+    title?: string;
+    type?: string;
+}
+
+export interface KeyedSchema {
+    [key: string]: JSONSchema;
+}
+
+export interface IDLObject {
+    contract_name: string;
+    contract_version: string;
+    idl_version: string;
+    instantiate: JSONSchema;
+    execute: JSONSchema;
+    query: JSONSchema;
+    migrate: JSONSchema;
+    sudo: JSONSchema;
+    responses: KeyedSchema;
+}
+
+export interface ContractInfo {
+    schemas: JSONSchema[];
+    responses?: Record<string, JSONSchema>;
+    idlObject?: IDLObject;
+};
