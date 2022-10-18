b9aeb10f4e0b8d6bec6b7ef87f43a045ccf83ddc
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-10-18 09:31:11 -0700

    feat: schema command (#139)
    Adding `terrain contract:schema` command.

---
+++ src/commands/contract/schema.ts
@@ -0,0 +1,31 @@
+import { Command } from '@oclif/command';
+import path from 'path';
+import runCommand from '../../lib/runCommand';
+import defaultErrorCheck from '../../lib/defaultErrorCheck';
+import TerrainCLI from '../../TerrainCLI';
+
+export default class ContractSchema extends Command {
+  static description = 'Generate contract schema.';
+
+  static args = [{ name: 'contract', required: true }];
+
+  async run() {
+    const { args } = this.parse(ContractSchema);
+
+    // Command execution path.
+    const execPath = path.join('contracts', args.contract);
+
+    // Command to be performed.
+    const command = async () => {
+      process.chdir(execPath);
+      TerrainCLI.runCargoCommand('schema');
+    };
+
+    // Attempt to execute command while backtracking through file tree.
+    await runCommand(
+      execPath,
+      command,
+      defaultErrorCheck(args.contract),
+    );
+  }
+}
